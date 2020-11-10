90bfc1cb33e6ce2226a6b27a3e04979cbcf8b435
fredMilloh <fred.milloh@hotmail.com>
2020-11-10 17:35:55 +0100

    add assets, create launchscreen

---
+++ ChatNoir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatNoir
+//
+//  Created by fred on 10/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
