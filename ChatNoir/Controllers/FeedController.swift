c3cda40bf7912fd801336daef730d0046f136746
fredMilloh <fred.milloh@hotmail.com>
2020-11-13 17:06:54 +0100

    create UI FeedController

--- ChatNoir/Controllers/FeedController.swift
+++ ChatNoir/Controllers/FeedController.swift
@@ -8,8 +8,24 @@
 import UIKit
 
 class FeedController: UIViewController {
-
+    
+    @IBOutlet weak var segmented: UISegmentedControl!
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var pencilButton: UIButton!
+    @IBOutlet weak var notifButton: UIButton!
+    @IBOutlet weak var profileButton: UIButton!
+    @IBOutlet weak var gearsButton: UIButton!
+    @IBOutlet weak var menuButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func segmentedPressed(_ sender: Any) {
+    }
+    @IBAction func menuPressed(_ sender: UIButton) {
+    }
+    
+    
 }
