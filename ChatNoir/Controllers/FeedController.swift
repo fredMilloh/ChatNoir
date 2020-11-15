6f32e0eb2bf5d84419efeb6418120bc5031d81d9
fredMilloh <fred.milloh@hotmail.com>
2020-11-15 18:03:25 +0100

    first animate menu open view

--- ChatNoir/Controllers/FeedController.swift
+++ ChatNoir/Controllers/FeedController.swift
@@ -19,12 +19,40 @@ class FeedController: UIViewController {
     @IBOutlet weak var gearsButton: UIButton!
     @IBOutlet weak var menuButton: UIButton!
     
+    var isMenuOpen = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func openMenu() {
+        menuView.translatesAutoresizingMaskIntoConstraints = true
+        menuButton.alpha = 0
+        UIView.animate(withDuration: 0.3) {
+            self.menuView.frame = CGRect(x: 20, y: self.menuView.frame.minY, width: self.view.frame.width - 40, height: 50)
+        } completion: { (success) in
+            UIView.animate(withDuration: 0.25) {
+                self.menuButton.alpha = 1
+                self.menuButton.transform = CGAffineTransform(scaleX: 1.25, y: 1.25)
+            } completion: { (success) in
+                //
+            }
+
+        }
+
+
+
+
+    }
+    
+    func closeMenu() {
+        
+        
     }
     @IBAction func segmentedPressed(_ sender: Any) {
     }
     @IBAction func menuPressed(_ sender: UIButton) {
+        isMenuOpen ? closeMenu() : openMenu()
     }
     
     
