e1d0cf1293eddda081801ab8d88aaafc222344e7
fredMilloh <fred.milloh@hotmail.com>
2020-11-17 17:56:18 +0100

    create container PostCell, implement delegate & datasource methods

--- ChatNoir/Controllers/ProfileController.swift
+++ ChatNoir/Controllers/ProfileController.swift
@@ -11,10 +11,44 @@ class ProfileController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    
+    var headerView: HeaderView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
     }
 
+}
+extension ProfileController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    // cell
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: POST_ID, for: indexPath) as? PostCell {
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 250)
+    }
+    
+    //Header
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: HEADER_ID, for: indexPath) as? HeaderView
+        return headerView!
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 200)
+    }
+    
 }
---
+++ ChatNoir/Views/Cells/PostCell.swift
@@ -0,0 +1,13 @@
+//
+//  PostCell.swift
+//  ChatNoir
+//
+//  Created by fred on 17/11/2020.
+//
+
+import UIKit
+
+class PostCell: UICollectionViewCell {
+    @IBOutlet weak var containerView: UIView!
+    
+}
