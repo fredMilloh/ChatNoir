8bbed8f06588ecd9446308b9bcb4cb7453c5d0fb
fred milloh <fred.milloh@hotmail.com>
2020-11-20 17:29:43 +0100

    WritePostView.xib UI

--- ChatNoir/Views/Loadable Views/WritePostView.swift
+++ ChatNoir/Views/Loadable Views/WritePostView.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class WritePostView: LoadableView {
 
+    @IBOutlet weak var descriptionTV: UITextView!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var imageTaken: UIImageView!
     
-
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func sendPressed(_ sender: Any) {
+    }
+    
+    @IBAction func takePicture(_ sender: Any) {
+    }
+    
+    
 }
